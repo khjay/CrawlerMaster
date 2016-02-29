599bc13114064ba12e2786e2b0fc3076313ad52f
khjay <sjzeng.tw@gmail.com>
2016-02-29 15:33:24 +0800

    get all depts

---
+++ UniversityCourse/MCU/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+from lxml import etree, html
+import requests
+
+if __name__ == "__main__":
+    result = requests.get("https://www.mcu.edu.tw/student/new-query/sel-query/query_3_up.asp")
+    root = etree.fromstring(result.text, etree.HTMLParser())
+    meta = root.xpath("//head/meta")[0]
+    charset = meta.get('content').split('=')[-1]
+    result.encoding = charset
+    depts = root.xpath("//select[@name='dept']/option")
+    deptsMap = []
+    for dept in depts:
+        deptsMap.append({'name': dept.text.strip().split(' - ')[1].encode('latin1').decode('big5'), 'value': dept.get('value')})
+    print(deptsMap)
