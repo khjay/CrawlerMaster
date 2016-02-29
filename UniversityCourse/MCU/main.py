b44d7a8193cfc44a365c6bae6b93e3c4e30d550e
khjay <sjzeng.tw@gmail.com>
2016-02-29 15:40:22 +0800

    move code to function()

--- UniversityCourse/MCU/main.py
+++ UniversityCourse/MCU/main.py
@@ -2,7 +2,11 @@
 from lxml import etree, html
 import requests
 
-if __name__ == "__main__":
+def main():
+    depts = getDepts()
+    print(depts)
+
+def getDepts():
     result = requests.get("https://www.mcu.edu.tw/student/new-query/sel-query/query_3_up.asp")
     root = etree.fromstring(result.text, etree.HTMLParser())
     meta = root.xpath("//head/meta")[0]
@@ -12,4 +16,7 @@ if __name__ == "__main__":
     deptsMap = []
     for dept in depts:
         deptsMap.append({'name': dept.text.strip().split(' - ')[1].encode('latin1').decode('big5'), 'value': dept.get('value')})
-    print(deptsMap)
+    return deptsMap
+
+if __name__ == "__main__":
+    main()
