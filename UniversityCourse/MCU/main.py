c37bcdc01b5e5c0d159fa068a3e99492fbc35ff2
khjay <sjzeng.tw@gmail.com>
2016-02-29 21:47:10 +0800

    get all courses

--- UniversityCourse/MCU/main.py
+++ UniversityCourse/MCU/main.py
@@ -6,8 +6,9 @@ from time import sleep
 def main():
     depts = getDepts()
     rawData = []
+
     for dept in depts:
-        course = getCourse(dept['value'], '1')
+        course = getCourse(dept['value'])
         deptData = {}
         deptData['科系'] = dept['name']
         if course != "no data":
@@ -20,18 +21,18 @@ def getCharset(root):
     return meta.get('content').split('=')[-1]
 
 def getDepts():
-    result = requests.get("https://www.mcu.edu.tw/student/new-query/sel-query/query_3_up.asp")
+    result = requests.get("http://www.mcu.edu.tw/student/new-query/sel-query/qslist.asp")
     root = etree.fromstring(result.text, etree.HTMLParser())
     result.encoding = getCharset(root)
-    depts = root.xpath("//select[@name='dept']/option")
+    depts = root.xpath("//select[@name='dept1']/option")
     deptsMap = []
-    for dept in depts:
+    for dept in depts[1:]:
         deptsMap.append({'name': latinToBig5(dept.text.strip().split(' - ')[1]), 'value': dept.get('value').strip()})
     return deptsMap
 
-def getCourse(dept, year):
+def getCourse(dept):
     courses = []
-    result = requests.get("https://www.mcu.edu.tw/student/new-query/sel-query/query_3_1.asp", data={'dept': dept, 'yr': year})
+    result = requests.get("http://www.mcu.edu.tw/student/new-query/sel-query/qslist_1.asp", data={'dept1': dept})
     root = etree.fromstring(result.text, etree.HTMLParser())
     result.encoding = getCharset(root)
     table = root.xpath("//table/tr")
@@ -47,11 +48,11 @@ def getCourse(dept, year):
             else:
                 tmp[latinToBig5(headers[i].text)] = latinToBig5(columns[i].text.strip())
         courses.append(tmp)
-    sleep(0.5)
+    sleep(1.5)
     return courses
 
 def latinToBig5(s):
-    return s.encode('latin1').decode('big5')
+    return s.encode('latin1', 'ignore').decode('big5', 'ignore')
         
 if __name__ == "__main__":
     main()
